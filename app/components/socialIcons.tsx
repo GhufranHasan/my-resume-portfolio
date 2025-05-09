aade285cc1f5e132f7b7066ef1d5d2ec28e8068a
Ghufran Hasan <ghufran.aims@gmail.com>
2025-05-09 10:04:34 +0500

    updated the linkedin url

--- app/components/socialIcons.tsx
+++ app/components/socialIcons.tsx
@@ -5,7 +5,7 @@ import { FaLinkedin, FaGithub, FaMedium, FaTwitter } from 'react-icons/fa'
 const SocialIcons = () => {
   return (
     <div className='top-[40vh] fixed font-content'>
-        <Link href={'https://www.linkedin.com/in/ghufran-hasan/'} target={'_blank'}>
+        <Link href={'https://www.linkedin.com/in/ghufranhasan/'} target={'_blank'}>
             <div className='ml-[-91px] hover:ml-0 duration-500 flex text-lg justify-between items-center fixed text-white bg-orange-800 w-36 py-3 px-5 rounded-r-lg'>LinkedIn <FaLinkedin size={30} /></div>
         </Link>
         <Link href={'https://github.com/GhufranHasan'} target={'_blank'}>
